5882a984de7574f5cb92c24a07512bf5883cdeff
chiruvamsi <kasachiruvamsi@gmail.com>
2025-06-09 23:34:32 +0530

    modify

--- main.tf
+++ main.tf
@@ -5,6 +5,6 @@ provider "aws" {
 }
 
 resource "aws_instance" "myfirstserverfromterra" {
-  ami           = "ami-095f54910906baa61"
-  instance_type = "t3.micro"
+  ami           = "ami-0a1b37d39fbc96e30"
+  instance_type = "t2.micro"
 }
