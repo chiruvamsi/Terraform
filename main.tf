29e4113e5b005aa597ac382ea31dd92dd5013918
chiruvamsi <kasachiruvamsi@gmail.com>
2025-06-09 23:08:13 +0530

    add main.tf file

---
+++ main.tf
@@ -0,0 +1,10 @@
+provider "aws" {
+    region = "us-east-1"
+    access_key = "AKIARWPFIU27ZXDBBHWS"
+    secret_key = "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"
+}
+
+resource "aws_instance" "myfirstserverfromterra" {
+ami = "ami-0a1b37d39fbc96e3"  
+instance_type = "t2.micro"
+}
